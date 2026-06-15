c856a5191fbb511af69ce43209ff39154ffb8905
daleadt <aditijagadale305@gmail.com>
2026-06-15 11:52:37 +0530

    uploaded

--- 14.2.py
+++ 14.2.py
@@ -1,6 +1,14 @@
 #example problem
 
-marks=input(int("Enter your marks: "))
-if marks>=80:
-    print("Grade A")
-    
+marks=int(input("Enter your marks: "))
+if (marks>=80):
+    if(marks>=90):
+        print("Top of the class")
+    else:
+        print("Grade A")
+elif marks>=60:
+    print("Grade B")
+elif marks>=40:
+    print("Grade C")
+else:
+    print("Grade D")
