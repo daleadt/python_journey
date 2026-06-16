f5fedb6ff1b5651d8d5547f5499b8f9905a3f507
daleadt <aditijagadale305@gmail.com>
2026-06-16 11:48:58 +0530

    uploaded

--- 15.1.py
+++ 15.1.py
@@ -2,8 +2,9 @@ a="hii, how are you?"
 print(a.count("i"))
 
 
-#using for loop to count 
+#using for loop to count
+count=0 
 for char in a:
     if char=="i":
-        count+=1
+        count+=1 # count = count + 1
 print(count)
