2709169d373b01aca5c67c2644451f5c96e75519
daleadt <aditijagadale305@gmail.com>
2026-06-17 11:01:41 +0530

    uploaded

--- 16.2.py
+++ 16.2.py
@@ -2,8 +2,8 @@
 #write a program to take input from user and check whether the string is starting with a vowel or not.
 
 a=input("Enter your word/sentence: ")
-
-if a[0] in "aeiouAEIOU":
+l=["a","e","i","o","u","A","E","I","O","U"]
+if a[0] in l:
     print("string starts with a vowel")
 else:
     print("string does not start with a vowel")
---
+++ 16.3.py
@@ -0,0 +1,5 @@
+#new line in python
+
+
+print("hello \nworld")
+print("Aditi enters\nhello everyone!! hope you're feeling great today")
